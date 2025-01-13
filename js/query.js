710b9ff8d868f2e5d1af4789469b20773091c962
michel <michel-gouveia1997@hotmail.com>
2025-01-13 17:08:27 -0300

    update

--- js/query.js
+++ js/query.js
@@ -11,3 +11,30 @@ function navigateToPage() {
 }
 
 
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	const menuIcon = document.querySelector(".mobile-menu-icon");
+	const mobileNav = document.querySelector(".mobile-nav");
+  
+	menuIcon.addEventListener("click", () => {
+	  mobileNav.classList.toggle("show");
+	  menuIcon.classList.toggle("active");
+	});
+  
+	menuIcon.addEventListener("transitionend", () => {
+	  const bars = menuIcon.querySelectorAll(".bar");
+	  if (menuIcon.classList.contains("active")) {
+		bars[0].style.transform = "rotate(45deg) translate(5px, 5px)";
+		bars[1].style.opacity = 0;
+		bars[2].style.transform = "rotate(-45deg) translate(5px, -5px)";
+	  } else {
+		bars.forEach((bar) => {
+		  bar.style.transform = "";
+		  bar.style.opacity = "";
+		});
+	  }
+	});
+  });
+  
